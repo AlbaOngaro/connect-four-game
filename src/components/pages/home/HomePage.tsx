305f9b4633762698adb5a2065b98967569f9fe40
Alba Ongaro <alba.ongaro@reprisk.com>
2023-07-31 12:51:58 +0200

    feat: added title + favicon

--- src/components/pages/home/HomePage.tsx
+++ src/components/pages/home/HomePage.tsx
@@ -5,6 +5,7 @@ import { useSelector } from "store/hooks";
 import { getMode } from "store/selectors";
 
 import { MenuContents } from "./contents/menu/MenuContents";
+import Head from "next/head";
 
 const DynamicGameContents = dynamic(
   () => import("./contents/game/GameContents").then((mod) => mod.GameContents),
@@ -18,9 +19,13 @@ export function HomePage() {
     mode: getMode(state),
   }));
 
-  if (mode !== null) {
-    return <DynamicGameContents />;
-  }
+  return (
+    <>
+      <Head>
+        <title>Connect Four</title>
+      </Head>
 
-  return <MenuContents />;
+      {mode !== null ? <DynamicGameContents /> : <MenuContents />}
+    </>
+  );
 }
